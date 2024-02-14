9213fa33b569560509fb0fb42c908eb2766eeeab
김도현 <dhkim@gimdohyeon-ui-MacBookAir.local>
2024-02-14 17:18:25 +0900

    fix inference error

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
-from .load_pretrained import FashionPoseEstimation
 from .load_pretrained import BodyPoseEstimation
 from .load_pretrained import FashionSegmentation
 from .load_pretrained import LadiVTON
